9f70b5ac88770f56d7defec18ffec3978319846c
caste23 <castellanos.xperta@gmail.com>
2022-03-09 22:21:53 -0600

    Incio del proyecto

---
+++ app/src/main/java/com/example/runninghn/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.runninghn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.runninghn.fragments.Frame_Iniciar;
+import com.example.runninghn.fragments.Frame_anuncio;
+import com.example.runninghn.fragments.Frame_main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar);
+
+
+    }
+}
