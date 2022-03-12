77fbd59ec7070b1f06a74b570df352c0d2331be4
caste23 <castellanos.xperta@gmail.com>
2022-03-12 14:40:22 -0600

    Nuevos activitis

--- app/src/main/java/com/example/runninghn/registrar.java
+++ app/src/main/java/com/example/runninghn/registrar.java
@@ -2,13 +2,49 @@ package com.example.runninghn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class registrar extends AppCompatActivity {
+    Button btnguardar;
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        btnguardar = findViewById(R.id.btnguardar);
+        imagen = findViewById(R.id.imageView);
+
+        btnguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamr();
+            }
+        });
     }
+    private void llamr(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(imgBitmap);
+        }
+    }
+
+
 }
